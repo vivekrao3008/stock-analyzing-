3ec1e31574c22e3ae9d14fbef7143fb9c5ec66bf
Vivek kumar <vivekkumar300804@gmail.com>
2026-09-21 23:34:34 +0530

    Fix lint errors: unused import, img alt

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -19,7 +19,7 @@ const Menu = () => {
 
   return (
     <div className="menu-container">
-      <img src="logo.png" style={{ width: "50px" }} />
+      <img src="/logo.png" alt="Logo" style={{ width: "50px" }} />
       <div className="menus">
         <ul>
           <li>
@@ -69,7 +69,7 @@ const Menu = () => {
           <li>
             <Link
               style={{ textDecoration: "none" }}
-              to="funds"
+              to="/funds"
               onClick={() => handleMenuClick(4)}
             >
               <p className={selectedMenu === 4 ? activeMenuClass : menuClass}>
